6fde99725ae808b5c2fd71da6f4f15d189f7eee2
Hussain <shaik.i.hussain@capgemini.com>
2025-09-02 17:14:49 +0530

    update uri

--- data/database.js
+++ data/database.js
@@ -6,7 +6,8 @@ const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
 //const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
- const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/${dbName}?retry
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/${dbName}?retryWrites=true&w=majority`;
+  
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
